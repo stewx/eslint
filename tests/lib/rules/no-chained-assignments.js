ee13743bb4008385b61798be42588c6d87cd6f73
Stewart Rand <srand@us.ibm.com>
2017-01-11 13:55:26 -0400

    working rule with unit tests

--- tests/lib/rules/no-chained-assignments.js
+++ tests/lib/rules/no-chained-assignments.js
@@ -44,6 +44,7 @@ ruleTester.run("no-chained-assignments", rule, {
     valid: [
         { code: "var a, b, c,\nd = 0;" },
         { code: "var a = 1; var b = 2; var c = 3;\nvar d = 0;" },
+        { code: "var a = 1 + (b === 10 ? 5 : 4);" },
         { code: "const a = 1, b = 2, c = 3;", parserOptions: { ecmaVersion: 6 } },
         { code: "const a = 1;\nconst b = 2;\n const c = 3;", parserOptions: { ecmaVersion: 6 } },
         { code: "for(var a = 0, b = 0;;){}" },
@@ -89,17 +90,15 @@ ruleTester.run("no-chained-assignments", rule, {
             errors: [
                 errorAt(1, 3, "AssignmentExpression"),
             ],
-            output: "a=c\nb=c;"
+            output: "a=c\nb=c"
         },
-
-        // Unfinished
 
         {
             code: "a\n=b\n=c",
             errors: [
                 errorAt(2, 2, "AssignmentExpression"),
             ],
-            output: "a\n=c\nb\n=c;"
+            output: "a\n=c\nb\n=c"
         },
 
         {
@@ -107,7 +106,7 @@ ruleTester.run("no-chained-assignments", rule, {
             errors: [
                 errorAt(1, 9, "AssignmentExpression"),
             ],
-            output: "var a = (((c)))\nb = (((c)));"
+            output: "var a = (((c)))\nb = (((c)))"
         },
 
         {
@@ -115,7 +114,15 @@ ruleTester.run("no-chained-assignments", rule, {
             errors: [
                 errorAt(1, 9, "AssignmentExpression"),
             ],
-            output: "var a = (c)\nb = (c);"
+            output: "var a = (c)\nb = (c)"
+        },
+
+        {
+            code: "var a = b = ( (c * 12) + 2)",
+            errors: [
+                errorAt(1, 9, "AssignmentExpression"),
+            ],
+            output: "var a = ( (c * 12) + 2)\nb = ( (c * 12) + 2)"
         },
 
         {
@@ -123,7 +130,15 @@ ruleTester.run("no-chained-assignments", rule, {
             errors: [
                 errorAt(2, 1, "AssignmentExpression"),
             ],
-            output: "var a = (c)\nb = (c);"
+            output: "var a =\n(c)\nb\n = (c)"
+        },
+
+        {
+            code: "a = b = '=' + c + 'foo';",
+            errors: [
+                errorAt(1, 5, "AssignmentExpression"),
+            ],
+            output: "a = '=' + c + 'foo';\nb = '=' + c + 'foo';"
         },
 
         {
